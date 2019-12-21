df549b724523c2724a8636722897bc6ae3a1c9e1
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2019-12-21 03:16:30 +0000

    "Basic serve structure"

--- libraryServer.js
+++ libraryServer.js
@@ -1,12 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 import express from 'express';
+import serveIndex from 'serve-index';
 
 import args from './args.js';
 import {say} from './common.js';
 
 const SITE_PATH = path.join(__dirname, 'public');
-const libraryPath = args.libraryPath || null;
+const libraryPath = args.libraryPath || path.join(__dirname, 'public', 'library');
 
 const app = express();
 
@@ -32,7 +33,7 @@ async function start({server_port}) {
 function addHandlers() {
   app.use(express.static(SITE_PATH));
   if ( !! libraryPath ) {
-    app.use("/library", express.static(libraryPath));
+    app.use("/library", express.static(libraryPath), serveIndex(libraryPath, {icons:true}));
   }
 }
 
--- public/library/README.md
+++ public/library/README.md
@@ -1,4 +1,4 @@
-# Default storage directory for library
+# ALT Default storage directory for library
 
 Remove `public/library/origin*` from `.gitignore` if you forked this repo and want to commit your library using git.
 
