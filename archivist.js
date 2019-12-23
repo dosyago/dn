ec4043f42f664279aff5715cec1e1bab953f795e
Stringfellow Cris <cris@dosyago.com>
2019-12-23 20:42:40 +0800

    Beginning refactor to have paths for extension and node.
    And also trying to keep as much common ground as possible.

--- archivist.js
+++ archivist.js
@@ -1,18 +1,16 @@
-import fs from 'fs';
-import ws from 'ws';
-import fetch from 'node-fetch';
+import {context} from './common.js';
+import {connect} from './protocol.js';
 
 // cache is a simple map
   // that holds the serialized requests
   // that are saved on disk
 const Cache = new Map();
-const ROOT_SESSION = 'browser';
 const State = {
   Cache
 }
 
 const Archivist = { 
-  collect, connect
+  collect 
 }
 
 const UNCACHED_BODY = Buffer.from('We have not saved this data').toString('base64');
@@ -22,10 +20,17 @@ const UNCACHED_HEADERS = [
   { name: 'Content-length', value: '26' }
 ];
 
+let Fs;
+
 export default Archivist;
 
 async function collect({chrome_port:port, mode} = {}) {
   const {send, on, ons} = await connect({port});
+
+  if ( context == 'node' ) {
+    const {default:fs} = await import('fs');
+    Fs = fs;
+  }
 
   // send commands and listen to events
     // so that we can intercept every request
@@ -45,7 +50,9 @@ async function collect({chrome_port:port, mode} = {}) {
     setInterval(saveCache, 10000);
   } else if ( mode == 'serve' ) {
     requestStage = "Request";
-    State.Cache = new Map(JSON.parse(fs.readFileSync('cache.json')));
+    if ( context == 'node' ) {
+      State.Cache = new Map(JSON.parse(Fs.readFileSync('cache.json')));
+    }
   } else {
     throw new TypeError(`Must specify mode`);
   }
@@ -108,100 +115,10 @@ async function collect({chrome_port:port, mode} = {}) {
   }
 
   function saveCache() {
-    fs.writeFileSync("cache.json", JSON.stringify([...State.Cache.entries()]));
+    if ( context == 'node' ) {
+      Fs.writeFileSync("cache.json", JSON.stringify([...State.Cache.entries()]));
+    }
   }
 }
 
-async function connect({port:port = 9222} = {}) {
-  const {webSocketDebuggerUrl} = await fetch(`http://localhost:${port}/json/version`).then(r => r.json());
-  const socket = new ws(webSocketDebuggerUrl);
-  const Resolvers = {};
-  const Handlers = {};
-  socket.on('message', handle);
-  let id = 0;
-  
-  async function send(method, params = {}, sessionId) {
-    const message = {
-      method, params, sessionId, 
-      id: ++id
-    };
-    const key = `${sessionId||ROOT_SESSION}:${message.id}`;
-    let resolve;
-    const promise = new Promise(res => resolve = res);
-    Resolvers[key] = resolve; 
-    socket.send(JSON.stringify(message));
-    return promise;
-  }
-
-  async function handle(message) {
-    const stringMessage = message;
-    message = JSON.parse(message);
-    if ( message.error ) {
-      console.warn(message);
-    }
-    const {sessionId} = message;
-    const {method, params} = message;
-    const {id, result} = message;
-
-    if ( id ) {
-      const key = `${sessionId||ROOT_SESSION}:${id}`;
-      const resolve = Resolvers[key];
-      if ( ! resolve ) {
-        console.warn(`No resolver for key`, key, stringMessage.slice(0,140));
-      } else {
-        Resolvers[key] = undefined;
-        try {
-          await resolve(result);
-        } catch(e) {
-          console.warn(`Resolver failed`, e, key, stringMessage.slice(0,140), resolve);
-        }
-      }
-    } else if ( method ) {
-      const listeners = Handlers[method];
-      if ( Array.isArray(listeners) ) {
-        for( const func of listeners ) {
-          try {
-            await func({message, sessionId});
-          } catch(e) {
-            console.warn(`Listener failed`, method, e, func.toString().slice(0,140), stringMessage.slice(0,140));
-          }
-        }
-      }
-    } else {
-      console.warn(`Unknown message on socket`, message);
-    }
-  }
-
-  function on(method, handler) {
-    let listeners = Handlers[method]; 
-    if ( ! listeners ) {
-      Handlers[method] = listeners = [];
-    }
-    listeners.push(wrap(handler));
-  }
-
-  function ons(method, handler) {
-    let listeners = Handlers[method]; 
-    if ( ! listeners ) {
-      Handlers[method] = listeners = [];
-    }
-    listeners.push(handler);
-  }
-
-  function wrap(fn) {
-    return ({message, sessionId}) => fn(message.params)
-  }
-
-  let resolve;
-  const promise = new Promise(res => resolve = res);
-
-  socket.on('open', () => resolve());
-
-  await promise;
-
-  return {
-    send,
-    on, ons
-  }
-}
 
--- common.js
+++ common.js
@@ -1,3 +1,22 @@
+// determine where this code is running 
+  let Context = 'unknown';
+
+  // ignore the possibility that window or global or chrome could be overwritten
+  const isBrowser = function () { try {return window && window.fetch;}catch(e){ return false;} };
+  const isNode = function () { try {return global && global.Math;}catch(e){return false;} };
+  const isExtension = function () { try {return chrome.runtime && chrome.debugger;}catch(e){return false;} };
+
+  if ( isNode() ) {
+    Context = 'node';
+  } else if ( isBrowser() ) {
+    Context = 'browser';
+    if ( isExtension() ) {
+      Context = 'extension';
+    }
+  }
+
+export const context = Context;
+
 export function say(o) {
   console.log(JSON.stringify(o));
 }
