985169f6357eb211fbd1de74ab1294012402cf5a
Stringfellow Cris <cris@dosyago.com>
2019-12-25 21:14:50 +0800

    Adding UI for mode selection

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ import Archivist from './archivist.js';
 import LibraryServer from './libraryServer.js';
 import args from './args.js';
 
-const CHROME_OPTS = DEBUG ? [
+const CHROME_OPTS = !DEBUG ? [] : [
   '--no-sandbox'
-] : []
-const server_port = process.env.PORT || args.server_port || 8080;
-const mode = args.mode;
+];
+const server_port = process.env.PORT || args.server_port || 22120;
+const mode = args.mode || 'save';
 const chrome_port = args.chrome_port || 9222;
 
 start();
