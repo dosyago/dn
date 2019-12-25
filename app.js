bbee56ad246e5e3bfe0d150f3c8c9ef1759e0022
Stringfellow Cris <cris@dosyago.com>
2019-12-26 00:09:06 +0800

    Configuring for pkg as binary.

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-import {launch as ChromeLaunch} from 'chrome-launcher';
-
-import {DEBUG} from './common.js';
+import {DEBUG, context} from './common.js';
 
 import Archivist from './archivist.js';
 import LibraryServer from './libraryServer.js';
@@ -10,11 +8,15 @@ const CHROME_OPTS = !DEBUG ? [] : [
   '--no-sandbox'
 ];
 const {server_port, mode, chrome_port} = args;
+let ChromeLaunch;
 
 start();
 
 async function start() {
-  //await ChromeLaunch({port: chrome_port, chromeFlags:CHROME_OPTS});
-  await LibraryServer.start({server_port});
+  if ( context == 'node' ) {
+    ({launch:ChromeLaunch} = await import('chrome-launcher'));
+    await ChromeLaunch({port: chrome_port, chromeFlags:CHROME_OPTS});
+    await LibraryServer.start({server_port});
+  }
   await Archivist.collect({chrome_port, mode});
 }
