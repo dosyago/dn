d09de863b627b6cd584e538dbb0c764d23f955bc
Stringfellow Cris <cris@dosyago.com>
2019-12-25 23:05:54 +0800

    Tested works as Node app and mode change via web UI.

--- libraryServer.js
+++ libraryServer.js
@@ -5,9 +5,10 @@ import serveIndex from 'serve-index';
 
 import args from './args.js';
 import {say} from './common.js';
+import Archivist from './archivist.js';
 
 const SITE_PATH = path.join(__dirname, 'public');
-export const libraryPath = args.libraryPath || path.join(__dirname, 'public', 'library');
+const libraryPath = args.library_path;
 
 const app = express();
 
@@ -31,9 +32,26 @@ async function start({server_port}) {
 }
 
 function addHandlers() {
+  const {chrome_port} = args;
+
+  app.use(express.urlencoded({extended:true}));
   app.use(express.static(SITE_PATH));
   if ( !! libraryPath ) {
     app.use("/library", express.static(libraryPath), serveIndex(libraryPath, {icons:true}));
   }
+
+  app.get('/search', async (req, res) => {
+    res.end('Not implemented yet');
+  });
+
+  app.get('/mode', async (req, res) => {
+    res.end(Archivist.getMode());
+  });
+
+  app.post('/mode', async (req, res) => {
+    const {mode} = req.body;
+    await Archivist.changeMode(mode);
+    res.end(`Mode set to ${mode}`);
+  });
 }
 
