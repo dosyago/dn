679bbfb71413ba3c1cf8fdf5122eeebdfa003d46
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-07-21 23:27:56 +0000

    New release

--- custom-launcher/chrome-launcher/index.js
+++
@@ -1,4 +0,0 @@
-// This provides legacy support for a previously documented require pattern
-//    const chromeLauncher = require('chrome-launcher/chrome-launcher');
-
-module.exports = require('../');
--- custom-launcher/scripts/format.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-FILES="`find src -type f -name '*.ts'`"
-
-./node_modules/.bin/clang-format -i -style=file $FILES
