82ce9b1082031598880be0a5f0d5d078feeffede
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2020-11-12 08:04:34 +0000

    New release

--- upload.sh
+++ upload.sh
@@ -4,11 +4,11 @@ gpush patch "New release"
 description=$1
 latest_tag=$(git describe --abbrev=0)
 grel release -u dosyago -r 22120 --tag $latest_tag --name "New release" --description '"'"$description"'"'
-grel upload -u dosyago -r 22120 --tag $latest_tag --name "22120.exe" --file 22120.exe
-grel upload -u dosyago -r 22120 --tag $latest_tag --name "22120.macos" --file 22120.mac
-grel upload -u dosyago -r 22120 --tag $latest_tag --name "22120.linux" --file 22120.nix
-grel upload -u dosyago -r 22120 --tag $latest_tag --name "22120.linx32" --file 22120.nix32
-grel upload -u dosyago -r 22120 --tag $latest_tag --name "22120.win32.exe" --file 22120.win32.exe
+grel upload -u c9fe -r 22120 --tag $latest_tag --name "22120.exe" --file 22120.exe
+grel upload -u c9fe -r 22120 --tag $latest_tag --name "22120.macos" --file 22120.mac
+grel upload -u c9fe -r 22120 --tag $latest_tag --name "22120.linux" --file 22120.nix
+grel upload -u c9fe -r 22120 --tag $latest_tag --name "22120.linx32" --file 22120.nix32
+grel upload -u c9fe -r 22120 --tag $latest_tag --name "22120.win32.exe" --file 22120.win32.exe
 
 
 
