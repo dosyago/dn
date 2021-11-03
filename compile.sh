34cc3baf3f5cd05603efe879afe6bc8cc32ec63d
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-11-03 08:38:55 +0000

    "Improving stability of build and packaging."

--- compile.sh
+++ compile.sh
@@ -7,4 +7,5 @@ nvm install v14.15.3
 nvm use v14.15.3
 
 npx webpack
+chmod +x 22120.js
 npx nexe -t windows -i 22120.js -r \"./?.22120.js\" -r \"./public/*\" && npx nexe -t linux-x64 -o 22120.nix -i 22120.js -r \"./?.22120.js\" -r \"./public/*\" && npx nexe -t macos-x64 -o 22120.mac -i 22120.js -r \"./?.22120.js\" -r \"./public/*\" && npx nexe -t windows-x32 -o 22120.win32.exe -i 22120.js -r \"./?.22120.js\" -r \"./public/*\" && npx nexe -t linux-x32 -o 22120.nix32 -i 22120.js -r \"./?.22120.js\" -r \"./public/*\"
