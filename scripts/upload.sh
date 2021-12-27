391f210450d1cb0acb1cc4f9a334e0d0622b571d
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-12-28 00:27:17 +0800

    New release
    Former-commit-id: 054301fd891a98d28c87ecb22a4ef892d698c53c

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,9 +4,9 @@ gpush patch "New release"
 description=$1
 latest_tag=$(git describe --abbrev=0)
 grel release -u i5ik -r 22120 --tag $latest_tag --name "New release" --description '"'"$description"'"'
-grel upload -u i5ik -r 22120 --tag $latest_tag --name "bin/exlibris-win.exe" --file exlibris-win.exe
-grel upload -u i5ik -r 22120 --tag $latest_tag --name "bin/exlibris-linux" --file exlibris-linux
-grel upload -u i5ik -r 22120 --tag $latest_tag --name "bin/exlibris-macos" --file exlibris-macos
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "exlibris-win.exe" --file bin/exlibris-win.exe
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "exlibris-linux" --file bin/exlibris-linux
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "exlibris-macos" --file bin/exlibris-macos
 
 
 
