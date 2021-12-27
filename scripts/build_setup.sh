6a93ed2a248f5bd0d15af1370fb6bd2b63fd1952
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-12-27 23:35:49 +0800

    "New build with pkg works!"
    Former-commit-id: 9e8e14844683abee5a678f5171951ce7366153df

--- scripts/build_setup.sh
+++ scripts/build_setup.sh
@@ -2,7 +2,10 @@
 
 echo "Installing nexe and upx..."
 
-npm i -g nexe
+mkdir -p bin/
+mkdir -p build/
+
+nexe || npm i -g nexe
 curl -L -o upx.tar.xz https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz
 tar -xJf upx.tar.xz
 rm upx.tar.xz
