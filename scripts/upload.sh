0a921f89c6bc2463049458133e66d202f5029200
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-12-28 00:18:48 +0800

    New release
    Former-commit-id: b8114a68495db10b20ee3422e7bc6fb71c4250c9

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -3,12 +3,10 @@
 gpush patch "New release" 
 description=$1
 latest_tag=$(git describe --abbrev=0)
-grel release -u dosyago -r 22120 --tag $latest_tag --name "New release" --description '"'"$description"'"'
-grel upload -u c9fe -r 22120 --tag $latest_tag --name "22120.exe" --file 22120.exe
-grel upload -u c9fe -r 22120 --tag $latest_tag --name "22120.macos" --file 22120.mac
-grel upload -u c9fe -r 22120 --tag $latest_tag --name "22120.linux" --file 22120.nix
-grel upload -u c9fe -r 22120 --tag $latest_tag --name "22120.linx32" --file 22120.nix32
-grel upload -u c9fe -r 22120 --tag $latest_tag --name "22120.win32.exe" --file 22120.win32.exe
+grel release -u i5ik -r 22120 --tag $latest_tag --name "New release" --description '"'"$description"'"'
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "bin/exlibris-win.exe" --file exlibris-win.exe
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "bin/exlibris-linux" --file exlibris-linux
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "bin/exlibris-macos" --file exlibris-macos
 
 
 
