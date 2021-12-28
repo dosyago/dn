7aeb5ff1ebe44f533872b0df8c8d62d3a71c9e5c
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-12-28 18:45:07 +0800

    patch

--- scripts/build_setup.sh
+++ scripts/build_setup.sh
@@ -5,18 +5,19 @@ echo "Installing nexe and upx..."
 mkdir -p bin/
 mkdir -p build/
 
-pkg || npm i -g pkg
+pkg -v || npm i -g pkg
 curl -L -o upx.tar.xz https://github.com/upx/upx/releases/download/v3.96/upx-3.96-amd64_linux.tar.xz
 tar -xJf upx.tar.xz
 rm upx.tar.xz
 sudo cp upx-3.96-amd64_linux/upx /usr/local/bin
 rm -rf upx-3.96-amd64_linux
 
-./dl-node.sh
+./scripts/dl-node.sh
 
-cd ~/.pkg-cache/v3.2/
-chmod +x *
-upx * || :
+# upx packing does not work with pkg
+#cd ~/.pkg-cache/v3.2/
+#chmod +x *
+#upx * || :
 
 echo "Done"
 
