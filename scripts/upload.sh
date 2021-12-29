9194c06c7aebc007c332dcc1b9640273d168fec3
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2021-12-30 01:21:03 +0800

    New release

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -4,9 +4,9 @@ gpush patch "New release"
 description=$1
 latest_tag=$(git describe --abbrev=0)
 grel release -u i5ik -r 22120 --tag $latest_tag --name "New release" --description '"'"$description"'"'
-grel upload -u i5ik -r 22120 --tag $latest_tag --name "exlibris-win.exe" --file bin/exlibris-win.exe
-grel upload -u i5ik -r 22120 --tag $latest_tag --name "exlibris-linux" --file bin/exlibris-linux
-grel upload -u i5ik -r 22120 --tag $latest_tag --name "exlibris-macos" --file bin/exlibris-macos
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "archivist1-win.exe" --file bin/archivist1-win.exe
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "archivist1-linux" --file bin/archivist1-linux
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "archivist1-macos" --file bin/archivist1-macos
 
 
 
