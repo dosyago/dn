c0653c899f7d851b10ee66b7f3a37424aa7757ff
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-01-02 05:09:05 +0800

    Select (bookmark) mode - archive by bookmarks. Fixes for Windows

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-gpush patch "New release" 
+./scripts/compile.sh
 description=$1
+gpush patch "$description" 
 latest_tag=$(git describe --abbrev=0)
 grel release -u i5ik -r 22120 --tag $latest_tag --name "New release" --description '"'"$description"'"'
 grel upload -u i5ik -r 22120 --tag $latest_tag --name "archivist1-win.exe" --file bin/archivist1-win.exe
