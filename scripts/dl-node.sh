0b598f43a897ffc7ba56fa244e1981809bc30f59
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-01-03 00:26:26 +0800

    "OK"

--- scripts/dl-node.sh
+++ scripts/dl-node.sh
@@ -4,8 +4,8 @@ unset npm_config_prefix
 source $HOME/.nvm/nvm.sh
 . $HOME/.profile
 
-nvm install v14.15.3
-nvm use v14.15.3
+nvm install --lts
+nvm use --lts
 
 pkg ./src/hello.js
 
--- scripts/release.sh
+++ scripts/release.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-./scripts/compile.sh
+#./scripts/compile.sh
 description=$1
 latest_tag=$(git describe --abbrev=0)
-npm version $latest_tag
-echo $(date) > .date.npm.release
-gpush patch "$description" 
+#npm version $latest_tag
+#echo $(date) > .date.npm.release
+#gpush patch "$description" 
 grel release -u i5ik -r 22120 --tag $latest_tag --name "New release" --description '"'"$description"'"'
 grel upload -u i5ik -r 22120 --tag $latest_tag --name "archivist1-win.exe" --file bin/archivist1-win.exe
 grel upload -u i5ik -r 22120 --tag $latest_tag --name "archivist1-linux" --file bin/archivist1-linux
