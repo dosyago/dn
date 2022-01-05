1099ab8e347ed17c7cc6decd257e00def0e67b98
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-01-05 13:46:35 +0800

    "OK"

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,20 +5,30 @@ source $HOME/.nvm/nvm.sh
 . $HOME/.profile
 nvm use --lts
 
-patch_required=$(grep -ER "require\([\"'](node:)?stream/web[\"']\)")
-if [[ ! -z "$patch_required" ]]; then
-  echo "Found an error!"
-  echo "Found something you need to patch before building"
-  echo "See: https://github.com/vercel/pkg/issues/1451"
-  echo
-  echo "$patch_required"
-  echo
-  echo "You need to add that to pkg.patches to replace with require('stream').web"
-  exit 1
-fi
-
 echo "Setting build mode..."
 ./scripts/go_build.sh
+
+patch_required=$(grep -ER "require\([\"'](node:)?stream/web[\"']\)" node_modules/*)
+files=$(grep -rlER "require\([\"'](node:)?stream/web[\"']\)" node_modules/*)
+if [[ ! -z "$patch_required" ]]; then
+  while IFS= read -r file; do
+    echo '--->' $file
+    grep -q $file package.json
+    if [ $? == 1 ]; then
+      echo '--->' $file "UNPATCHED!"
+      echo "Found an error!"
+      echo "Found something you need to patch before building"
+      echo "See: https://github.com/vercel/pkg/issues/1451"
+      echo
+      echo "$patch_required"
+      echo
+      echo "You need to add all these to pkg.patches to replace with require('stream').web"
+      ./scripts/go_dev.sh
+      exit 1
+    fi
+    echo "OK"
+  done <<< $files
+fi
 
 npm run bundle
 
