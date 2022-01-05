a9ded5b6ff31d6856031ebcd0ed3c56f5c5615d2
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-01-05 17:30:58 +0800

    "Improving release script"

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,13 +3,10 @@
 #./scripts/compile.sh
 description=$1
 latest_tag=$(git describe --abbrev=0)
-npm version $latest_tag
-#echo $(date) > .date.npm.release
-#gpush patch "$description" 
 grel release -u i5ik -r 22120 --tag $latest_tag --name "New release" --description '"'"$description"'"'
-grel upload -u i5ik -r 22120 --tag $latest_tag --name "archivist1-win.exe" --file bin/archivist1-win.exe
-grel upload -u i5ik -r 22120 --tag $latest_tag --name "archivist1-linux" --file bin/archivist1-linux
-grel upload -u i5ik -r 22120 --tag $latest_tag --name "archivist1-macos" --file bin/archivist1-macos
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "diskernet-win.exe" --file bin/diskernet-win.exe
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "diskernet-linux" --file bin/diskernet-linux
+grel upload -u i5ik -r 22120 --tag $latest_tag --name "diskernet-macos" --file bin/diskernet-macos
 
 
 
