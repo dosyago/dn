fb45f70ce307c389cb75de9d77233fb079b9b74c
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-02-26 13:36:05 +0800

    "Waits are working"

--- src/common.js
+++ src/common.js
@@ -4,14 +4,20 @@ import {fileURLToPath} from 'url';
 export const DEBUG = process.env.DEBUG_22120 || false;
 export const SHOW_FETCH = false;
 
+// crawl related
+export const MIN_TIME_PER_PAGE = 7000;
+export const MAX_TIME_PER_PAGE = 32000;
+export const MIN_WAIT = 1000;
+export const MAX_WAITS = 60;
+export const BATCH_SIZE = 1; // crawl batch size (how many concurrent tabs for crawling)
+export const MAX_REAL_URL_LENGTH = 2**15 - 1;
+
 export const CHECK_INTERVAL = 400;
 export const TEXT_NODE = 3;
 export const MAX_HIGHLIGHTABLE_LENGTH = 0;    /* 0 is no max length for highlight */
 export const MAX_TITLE_LENGTH = 140;
 export const MAX_URL_LENGTH = 140;
-export const MAX_REAL_URL_LENGTH = 2**15 - 1;
 export const MAX_HEAD = 140;
-export const BATCH_SIZE = 1; // crawl batch size (how many concurrent tabs for crawling)
 
 export class RichError extends Error {
   constructor(msg) {
@@ -66,4 +72,27 @@ export function say(o) {
 
 export function clone(o) {
   return JSON.parse(JSON.stringify(o));
+}
+
+export async function untilTrue(pred, waitOverride = MIN_WAIT, maxWaits = MAX_WAITS) {
+  console.log(waitOverride, maxWaits);
+  let waitCount = 0;
+  let resolve;
+  const pr = new Promise(res => resolve = res);
+  setTimeout(checkPred, 0);
+  return pr;
+
+  function checkPred() {
+    console.log('Checking', pred);
+    if ( pred() ) {
+      return resolve(true);
+    } else {
+      waitCount++;
+      if ( waitCount < maxWaits ) {
+        setTimeout(checkPred, waitOverride);
+      } else {
+        resolve(false);
+      }
+    }
+  }
 }
