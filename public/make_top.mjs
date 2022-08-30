d2b3a3d1ade678112e3016c68941cd80fd502cf9
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2022-08-30 20:23:26 +0800

    Good run

--- public/make_top.mjs
+++ public/make_top.mjs
@@ -22,6 +22,71 @@ process.on('beforeExit', cleanup);
 
 make();
 
+async function make() {
+  const titlesFile = fs.readFileSync(path.resolve('.', 'topTitles.json')).toString();
+  const titles = new Map(JSON.parse(titlesFile).map(([url, title]) => [url, {url,title}]));
+  if ( CLEAN ) {
+    for ( const [url, obj] of titles ) {
+      const k1 = clean(url);
+      const k2 = clean2(url);
+      if ( !titles.has(k1) ) {
+        titles.set(k1, obj);
+      }
+      if ( !titles.has(k2) ) {
+        titles.set(k2, obj);
+      }
+    }
+  }
+  const remainingFile =  fs.readFileSync(path.resolve('.', 'remainingFile.json')).toString();
+  const remainingSet = new Set(JSON.parse(remainingFile));
+  const countsFile = fs.readFileSync(path.resolve('.', 'ran-counts.json')).toString();
+  counts = new Map(JSON.parse(countsFile).filter(([url, count]) => remainingSet.has(url)));
+  let current = 0;
+  for ( const [url, count] of counts ) {
+    let title;
+    let realUrl;
+    if ( titles.has(url) ) {
+      ({title} = titles.get(url));
+      entries.push({
+        url, 
+        title, 
+        count,
+      });
+      counted.add(url);
+    } else {
+      console.log(`Curl call for ${url} in progress...`);
+      let notifyCurlComplete;
+      const curlCall = new Promise(res => notifyCurlComplete = res);
+      do {
+        await sleep(1000);
+      } while ( current >= CONCURRENT );
+      child_process.exec(curlCommand(url), (err, stdout, stderr) => {
+        if ( ! err && (!stderr || stderr.length == 0)) {
+          realUrl = stdout; 
+          if ( titles.has(realUrl) ) {
+            ({title} = titles.get(realUrl));
+            entries.push({
+              url, 
+              realUrl,
+              title, 
+              count,
+            });
+            counted.add(url);
+          }
+        } else {
+          console.log(`Error on curl for ${url}`, {err, stderr});
+          errors.set(url, {err, stderr});
+        }
+        console.log(`Curl call for ${url} complete!`);
+        notifyCurlComplete();
+      });
+      current += 1;
+      curlCall.then(() => current -= 1);
+    }
+  }
+  cleanup();
+}
+
 async function make_v2() {
   const titlesFile = fs.readFileSync(path.resolve('.', 'topTitles.json')).toString();
   const titles = new Map(JSON.parse(titlesFile).map(([url, title]) => [url, {url,title}]));
@@ -91,7 +156,7 @@ function cleanup() {
   console.log('cleanup running');
   if ( errors.size ) {
     fs.writeFileSync(
-      path.resolve('.', 'errorLinks.json'),
+      path.resolve('.', 'errorLinks4.json'),
       JSON.stringify([...errors.keys()], null, 2)
     );
     console.log(`Wrote errors`);
@@ -99,13 +164,13 @@ function cleanup() {
   if ( counted.size !== counts.size ) {
     counted.forEach(url => counts.delete(url)); 
     fs.writeFileSync(
-      path.resolve('.', 'noTitleFound.json'),
+      path.resolve('.', 'noTitleFound4.json'),
       JSON.stringify([...counts.keys()], null, 2)
     )
     console.log(`Wrote noTitleFound`);
   }
   fs.writeFileSync(
-    path.resolve('.', 'topFrontPageLinksWithCounts.json'), 
+    path.resolve('.', 'topFrontPageLinksWithCounts4.json'), 
     JSON.stringify(entries, null, 2)
   );
   console.log(`Wrote top links with counts`);
@@ -164,7 +229,7 @@ function clean2(urlString) {
 }
 
 function curlCommand(url) {
-  return `curl -L -s -o /dev/null -w '%{url_effective}' ${url} \
+  return `curl -k -L -s -o /dev/null -w '%{url_effective}' ${JSON.stringify(url)} \
     -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
     -H 'Accept-Language: en,en-US;q=0.9,zh-TW;q=0.8,zh-CN;q=0.7,zh;q=0.6,ja;q=0.5' \
     -H 'Cache-Control: no-cache' \
