4180ffb202863023b7c6c28b2940ab82ac751a48
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-01-11 11:45:27 +0800

    Just built

--- src/common.js
+++ src/common.js
@@ -1,21 +1,8 @@
 import path from 'path';
-import {fileURLToPath} from 'url';
-import fs from 'fs';
-import os from 'os';
+//import {fileURLToPath} from 'url';
 
 export const DEBUG = process.env.DEBUG_22120 || false;
 export const SHOW_FETCH = false;
-
-// server related
-export const PUBLIC_SERVER = true;
-
-// crawl related
-export const MIN_TIME_PER_PAGE = 10000;
-export const MAX_TIME_PER_PAGE = 32000;
-export const MIN_WAIT = 1000;
-export const MAX_WAITS = 60;
-export const BATCH_SIZE = 5; // crawl batch size (how many concurrent tabs for crawling)
-export const MAX_REAL_URL_LENGTH = 2**15 - 1;
 
 export const CHECK_INTERVAL = 400;
 export const TEXT_NODE = 3;
@@ -23,21 +10,6 @@ export const MAX_HIGHLIGHTABLE_LENGTH = 0;    /* 0 is no max length for highligh
 export const MAX_TITLE_LENGTH = 140;
 export const MAX_URL_LENGTH = 140;
 export const MAX_HEAD = 140;
-
-export const GO_SECURE = fs.existsSync(path.resolve(os.homedir(), 'local-sslcerts', 'privkey.pem'));
-
-export class RichError extends Error {
-  constructor(msg) {
-    let textMessage;
-    try {
-      textMessage = JSON.stringify(msg);
-    } catch(e) {
-      console.warn(`Could not create RichError from argument ${msg.toString ? msg.toString() : msg} as JSON serialization failed. RichError argument MUST be JSON serializable. Failure error was:`, e);
-      return;
-    }
-    super(textMessage);
-  }
-}
 
 /* text nodes inside these elements that are ignored */
 export const FORBIDDEN_TEXT_PARENT = new Set([
@@ -65,11 +37,11 @@ export const ERROR_CODE_SAFE_TO_IGNORE = new Set([
 
 export const SNIP_CONTEXT = 31;
 
-export const NO_SANDBOX = (process.env.DEBUG_22120 && process.env.SET_22120_NO_SANDBOX) || false;
+export const NO_SANDBOX = process.env.DEBUG_22120 || false;
 
 //export const APP_ROOT = '.';
-//export const APP_ROOT = __dirname;
-export const APP_ROOT = path.dirname(fileURLToPath(import.meta.url));
+export const APP_ROOT = __dirname;
+//export const APP_ROOT = path.dirname(fileURLToPath(import.meta.url));
 
 export const sleep = ms => new Promise(res => setTimeout(res, ms));
 
@@ -79,30 +51,4 @@ export function say(o) {
 
 export function clone(o) {
   return JSON.parse(JSON.stringify(o));
-}
-
-export async function untilTrue(pred, waitOverride = MIN_WAIT, maxWaits = MAX_WAITS) {
-  if ( waitOverride < 0 ) {
-    maxWaits = -1;
-    waitOverride = MIN_WAIT;
-  }
-  let waitCount = 0;
-  let resolve;
-  const pr = new Promise(res => resolve = res);
-  setTimeout(checkPred, 0);
-  return pr;
-
-  async function checkPred() {
-    DEBUG && console.log('Checking', pred.toString());
-    if ( await pred() ) {
-      return resolve(true);
-    } else {
-      waitCount++;
-      if ( waitCount < maxWaits || maxWaits < 0 ) {
-        setTimeout(checkPred, waitOverride);
-      } else {
-        resolve(false);
-      }
-    }
-  }
 }
