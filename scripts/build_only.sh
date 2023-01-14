44dd74a86f4c0d32c28c91c098157b1f92d65402
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-01-14 17:57:37 +0800

    New

--- scripts/build_only.sh
+++ scripts/build_only.sh
@@ -2,7 +2,8 @@
 
 ./node_modules/.bin/esbuild src/app.js --bundle --outfile=dist/diskernet.mjs --format=esm --platform=node --minify --analyze
 ./node_modules/.bin/esbuild src/app.js --bundle --outfile=build/out.cjs --platform=node --minify --analyze
-echo "#!/usr/bin/env node" > build/diskernet.js
-cat build/out.cjs >> build/diskernet.js
-chmod +x build/diskernet.js
+./node_modules/.bin/esbuild src/app.js --bundle --outfile=build/test.cjs --platform=node 
+echo "#!/usr/bin/env node" > build/diskernet.cjs
+cat build/out.cjs >> build/diskernet.cjs
+chmod +x build/diskernet.cjs
 
