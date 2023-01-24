37b70520390c4da57862b06706bd27b4c28a2ee2
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-01-25 01:03:36 +0800

    cannot sign and set icon

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -9,7 +9,6 @@ nvm use --lts
 
 pkg --compress GZip .
 
-xattr -cr .
 iconset single ./icons/dk.icns bin/diskernet-macos
 codesign -vvvv --timestamp -s "Developer ID Application" --force bin/diskernet-macos
 
