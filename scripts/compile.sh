868993a14ef24fcdb867416a7ee144e7c2f3de34
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-01-25 00:57:53 +0800

    Adding icons to macos

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -6,7 +6,10 @@ unset npm_config_prefix
 source $HOME/.nvm/nvm.sh
 nvm use --lts
 
+
 pkg --compress GZip .
 
+xattr -cr .
+iconset single ./icons/dk.icns bin/diskernet-macos
 codesign -vvvv --timestamp -s "Developer ID Application" --force bin/diskernet-macos
 
