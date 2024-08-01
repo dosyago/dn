c641fea53096c9839dc3c78315f49d3bfb010cb7
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-08-01 12:23:55 +0800

    Build updated

--- scripts/build_only.sh
+++ scripts/build_only.sh
@@ -6,6 +6,7 @@ rm -rf build
 mkdir -p build/esm/
 mkdir -p build/cjs/
 mkdir -p build/global/
+mkdir -p build/bin/
 nvm use v22
 if [[ ! -d "node_modules" ]]; then
   npm i
@@ -16,10 +17,10 @@ echo "#!/usr/bin/env node" > build/global/downloadnet.cjs
 cat build/cjs/out.cjs >> build/global/downloadnet.cjs
 chmod +x build/global/downloadnet.cjs
 if [[ "$OSTYPE" == darwin* ]]; then
-  ./stampers/macos.sh
+  ./stampers/macos.sh dn build/cjs/out.cjs build/bin/
 elif [[ "$OSTYPE" == win* ]]; then
-  ./stampers/win.sh
+  ./stampers/win.sh dn build/cjs/out.cjs build/bin/
 else
-  ./stampers/nix.sh
+  ./stampers/nix.sh build/cjs/out.cjs build/bin/
 fi
 
