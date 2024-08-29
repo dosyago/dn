80e6667f4111e3832cb6eaa0d0dff076ab94bd1f
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2024-08-29 22:33:26 +0800

    Fix win

--- stampers/win.bat
+++ stampers/win.bat
@@ -43,7 +43,7 @@ node -e "require('fs').copyFileSync(process.execPath, '%OUTPUT_FOLDER%\%EXE_NAME
 signtool remove /s "%OUTPUT_FOLDER%\%EXE_NAME%"
 
 :: Inject the blob into the copied binary
-npx postject "%OUTPUT_FOLDER%\%EXE_NAME%" NODE_SEA_BLOB sea-prep.blob --sentinel-fuse %NODE_SEA_FUSE%
+npx postject "%OUTPUT_FOLDER%\%EXE_NAME%" NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
 
 :: Clean up
 echo Application built successfully.
